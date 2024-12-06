8db27b15e22bec3bd60fcf1bc605a8d79bc39805
SussyPusya <mranimatory@gmal.com>
2024-12-06 20:54:20 +0300

    Make http server

--- internal/app/application.go
+++ internal/app/application.go
@@ -1,1 +1,44 @@
 package app
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+type Config struct {
+	Path string
+}
+
+func ConfigWhitEnv() *Config {
+	cfg := new(Config)
+
+	cfg.Path = os.Getenv("PORT")
+	if cfg.Path == "" {
+		cfg.Path = "8080"
+
+	}
+	return cfg
+}
+
+// Application struct
+type App struct {
+	config *Config
+}
+
+func New() *App {
+	return &App{
+		config: ConfigWhitEnv(),
+	}
+}
+
+func (a *App) Run() error {
+
+}
+
+func (a *App) RunServer() error {
+	mux := mux.NewRouter()
+
+	http.ListenAndServe(":"+a.config.Path, mux)
+}
